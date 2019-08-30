769dbc416119f188f77bb2b91492b0b2d2c43882
blankia1 <54330617+blankia1@users.noreply.github.com>
2019-08-31 00:55:46 +0200

    Update tests.py

--- src/todo/tests.py
+++ src/todo/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from todo.models import TodoItem
 
+#change file
 # Create your tests here.
 def createItem(client):
   url = reverse('todoitem-list')
